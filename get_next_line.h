8beab889bff0ef82648382cf3cada722b111665e
Markus Kurz <makurz@3-B-11.42heilbronn.de>
2023-04-05 18:51:06 +0200

    added header files inital values

--- get_next_line.h
+++ get_next_line.h
@@ -6,15 +6,17 @@
 /*   By: makurz <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/05 10:59:23 by makurz            #+#    #+#             */
-/*   Updated: 2023/04/05 11:02:36 by makurz           ###   ########.fr       */
+/*   Updated: 2023/04/05 18:50:52 by makurz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-#include <fcntl.h>
-#include <stdlib.h>
-#include <stddef.h>
+# include <fcntl.h>
+# include <stdlib.h>
+# include <stddef.h>
 
-#endif // !GET_NEXT_LINE_H
+char *get_next_line(int fd);
+
+#endif
---
+++ get_next_line_bonus.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: makurz <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/05 18:49:56 by makurz            #+#    #+#             */
+/*   Updated: 2023/04/05 18:50:41 by makurz           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <fcntl.h>
+# include <stdlib.h>
+# include <stddef.h>
+
+char *get_next_line(int fd);
+
+#endif
