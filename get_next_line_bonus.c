ab5bea6931329cbc3e3f11c761d89a9b8e613cbe
kurz-m <m.kurz92@gmail.com>
2023-04-09 10:48:19 +0200

    Updated bonus header; adjusted variables for bonus.

--- get_next_line_bonus.c
+++ get_next_line_bonus.c
@@ -1,31 +1,35 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*   get_next_line_bonus.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: makurz <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/08 16:13:31 by makurz            #+#    #+#             */
-/*   Updated: 2023/04/08 23:40:45 by makurz           ###   ########.fr       */
+/*   Updated: 2023/04/09 10:47:56 by makurz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
+#include "get_next_line_bonus.h"
 
+// Main function body of get_next_line
+// First 'if' statement checks for valid inputs
+// While loop is there for creating the new new_line
+// and also filling the stash for keeping string after the new_line
 char	*get_next_line(int fd)
 {
-	static char	stash[BUFFER_SIZE + 1];
+	static char	stash[MAX_FD][BUFFER_SIZE + 1];
 	t_gnl		next;
 
-	next.new_line = NULL;
-	if (fd < 0 || BUFFER_SIZE < 1)
+	if (fd < 0 || BUFFER_SIZE < 1 || fd > MAX_FD)
 		return (NULL);
-	while (stash[0] != '\0' || read(fd, stash, BUFFER_SIZE) > 0)
+	next.new_line = NULL;
+	while (stash[fd][0] != '\0' || read(fd, stash[fd], BUFFER_SIZE) > 0)
 	{
-		next.new_line = ft_strjoin(next.new_line, stash, next);
-		if (ft_clean_stash(stash, next) == TRUE)
+		next.new_line = ft_strjoin(next.new_line, stash[fd], next);
+		if (ft_clean_stash(stash[fd], next) == TRUE)
 			break ;
-		if (read(fd, stash, 0) < 0)
+		if (read(fd, stash[fd], 0) < 0)
 		{
 			free(next.new_line);
 			return (NULL);
--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -1,17 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: makurz <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/08 00:09:22 by makurz            #+#    #+#             */
-/*   Updated: 2023/04/08 23:40:29 by makurz           ###   ########.fr       */
+/*   Updated: 2023/04/09 10:43:34 by makurz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
 
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 42
@@ -19,6 +19,10 @@
 
 # define TRUE 1
 # define FALSE 0
+
+# ifndef MAX_FD
+#  define MAX_FD 42
+# endif
 
 # include <unistd.h>
 # include <stdlib.h>
