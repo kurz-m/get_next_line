1eb2a5ba41de2db476470978eea96771a11375e8
Markus Kurz <makurz@3-B-11.42heilbronn.de>
2023-04-11 13:21:31 +0200

    checked FOPEN_MAX for 42 computers

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: makurz <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/06 08:40:41 by makurz            #+#    #+#             */
-/*   Updated: 2023/04/10 15:18:48 by makurz           ###   ########.fr       */
+/*   Updated: 2023/04/11 09:10:21 by makurz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,19 +27,25 @@
 // 	return 0;
 // }
 
-#include <fcntl.h>
-
+// #include <fcntl.h>
+// 
+// int	main(void)
+// {
+// 	int		fd;
+// 	char	*line;
+// 
+// 	line = NULL;
+// 	fd = open("../get_next_line_tests/multiple_new_line.txt", O_RDONLY);
+// 	while ((line = get_next_line(fd)) != NULL)
+// 	{
+// 		printf("new line: #%s", line);
+// 		free(line);
+// 	}
+// 	return (0);
+// }
+//
 int	main(void)
 {
-	int		fd;
-	char	*line;
-
-	line = NULL;
-	fd = open("../get_next_line_tests/multiple_new_line.txt", O_RDONLY);
-	while ((line = get_next_line(fd)) != NULL)
-	{
-		printf("new line: #%s", line);
-		free(line);
-	}
+	printf("%i", FOPEN_MAX);
 	return (0);
 }
